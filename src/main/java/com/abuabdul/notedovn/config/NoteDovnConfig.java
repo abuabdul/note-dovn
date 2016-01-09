340b2fecf77b80199748c675dcd360f3e56a3d2e
abuabdul <abuabdul86@hotmail.com>
2016-01-09 17:03:14 +0530

    mongodb is configured

--- src/main/java/com/abuabdul/notedovn/config/NoteDovnConfig.java
+++ src/main/java/com/abuabdul/notedovn/config/NoteDovnConfig.java
@@ -1,9 +1,53 @@
 package com.abuabdul.notedovn.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+
+import com.mongodb.MongoClient;
+
 /**
  * @author abuabdul
  *
  */
 public class NoteDovnConfig {
 
+	private static final String APPCONFIG_FILE_NAME = "Appconfig.properties";
+
+	@Value("${mongo.host}")
+	private String mongohost;
+	@Value("${mongo.port}")
+	private int mongoport;
+	@Value("${mongo.databaseName}")
+	private String mongodbName;
+
+	@Bean
+	public static PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
+		PropertyPlaceholderConfigurer propertyConfigurer = new PropertyPlaceholderConfigurer();
+		propertyConfigurer.setLocation(new ClassPathResource(APPCONFIG_FILE_NAME));
+		propertyConfigurer.setIgnoreResourceNotFound(false);
+		propertyConfigurer.setIgnoreUnresolvablePlaceholders(true);
+		propertyConfigurer.setSearchSystemEnvironment(true);
+		propertyConfigurer.setSystemPropertiesMode(PropertyPlaceholderConfigurer.SYSTEM_PROPERTIES_MODE_OVERRIDE);
+		return propertyConfigurer;
+	}
+
+	@Bean
+	public MongoTemplate mongoTemplate() throws Exception {
+		return new MongoTemplate(mongoDbFactory());
+	}
+
+	@Bean
+	public MongoDbFactory mongoDbFactory() throws Exception {
+		return new SimpleMongoDbFactory(mongoClient(), mongodbName);
+	}
+
+	@Bean
+	public MongoClient mongoClient() throws Exception {
+		return new MongoClient(mongohost, mongoport);
+	}
 }
