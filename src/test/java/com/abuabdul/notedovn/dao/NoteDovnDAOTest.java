a3ef17f1e7df2c9752de39002dc8224e3d678f27
abuabdul <abuabdul86@hotmail.com>
2016-01-17 17:19:20 +0530

    Code clean up and licensing

--- src/test/java/com/abuabdul/notedovn/dao/NoteDovnDAOTest.java
+++ src/test/java/com/abuabdul/notedovn/dao/NoteDovnDAOTest.java
@@ -1,6 +1,21 @@
+/*
+ * Copyright 2013-2016 abuabdul.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
 package com.abuabdul.notedovn.dao;
 
-import static com.abuabdul.notedovn.util.NoteDovnUtil.getUTCDateTime;
 import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.verify;
 import static org.springframework.data.mongodb.core.query.Criteria.where;
@@ -57,9 +72,6 @@ public class NoteDovnDAOTest {
 	@Test(groups = "integration")
 	public void testUpdateNote() throws Exception {
 		noteDovnDAO.updateNote("pk", "name", "someValue");
-		Update update = new Update();
-		update.set("name", "someValue");
-		update.set("updatedDate", getUTCDateTime());
 		verify(mongoTemplate).updateFirst(any(Query.class), any(Update.class), any(Class.class));
 	}
 
--- src/test/java/com/abuabdul/notedovn/service/NoteDovnServiceTest.java
+++ src/test/java/com/abuabdul/notedovn/service/NoteDovnServiceTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2013-2016 abuabdul.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
 package com.abuabdul.notedovn.service;
 
 import static org.mockito.Mockito.verify;
@@ -24,6 +40,9 @@ public class NoteDovnServiceTest {
 	@InjectMocks
 	private NoteDovnServiceImpl noteDovnService;
 
+	@Mock
+	private ScratchNote note;
+
 	@BeforeClass
 	public void initMocks() {
 		MockitoAnnotations.initMocks(this);
@@ -31,14 +50,12 @@ public class NoteDovnServiceTest {
 
 	@Test(groups = "integration")
 	public void testMakeScratchNote() throws NoteDovnServiceException {
-		ScratchNote note = new ScratchNote();
 		noteDovnService.makeScratchNote(note);
 		verify(noteDovnDAO).saveNote(note);
 	}
 
 	@Test(groups = "integration")
 	public void testStrikeScratchNote() throws NoteDovnServiceException {
-		ScratchNote note = new ScratchNote();
 		noteDovnService.strikeScratchNote(note);
 		verify(noteDovnDAO).removeNote(note);
 	}
