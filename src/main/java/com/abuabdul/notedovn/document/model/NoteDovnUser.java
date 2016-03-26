58a7158071c6e04e0ab17b2d70971c8d2444bf32
abuabdul <abuabdul86@hotmail.com>
2016-03-26 23:26:19 +0530

    Spring security Userdetails service

---
+++ src/main/java/com/abuabdul/notedovn/document/model/NoteDovnUser.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright 2013-2016 abuabdul.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package com.abuabdul.notedovn.document.model;
+
+/**
+ * @author abuabdul
+ *
+ */
+public class NoteDovnUser {
+
+	private String id;
+	private String username;
+	private String password;
+	private String roles;
+
+	/**
+	 * @return the id
+	 */
+	public String getId() {
+		return id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username
+	 *            the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password
+	 *            the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the roles
+	 */
+	public String getRoles() {
+		return roles;
+	}
+
+	/**
+	 * @param roles
+	 *            the roles to set
+	 */
+	public void setRoles(String roles) {
+		this.roles = roles;
+	}
+}
---
+++ src/main/java/com/abuabdul/notedovn/service/NoteDovnUserDetailsService.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2013-2016 abuabdul.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ */
+package com.abuabdul.notedovn.service;
+
+import static org.springframework.data.mongodb.core.query.Criteria.where;
+
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.abuabdul.notedovn.document.model.NoteDovnUser;
+
+/**
+ * @author abuabdul
+ *
+ */
+public class NoteDovnUserDetailsService implements UserDetailsService {
+
+	private final MongoTemplate mongoTemplate;
+
+	/**
+	 * @param mongoTemplate
+	 */
+	public NoteDovnUserDetailsService(MongoTemplate mongoTemplate) {
+		this.mongoTemplate = mongoTemplate;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		mongoTemplate.findOne(query(username), NoteDovnUser.class);
+		return null;
+	}
+
+	protected Query query(String username) {
+		return new Query(where("username").is(username));
+	}
+
+}
